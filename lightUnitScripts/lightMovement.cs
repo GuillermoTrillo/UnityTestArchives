136a7ed73083c38ffae96c9c56f6dfd60367a236
Guillermo del Toro Trillo <guillermotoro61@gmail.com>
2024-10-04 12:36:23 -0300

    Light movement complete and commented

--- lightUnitScripts/lightMovement.cs
+++ lightUnitScripts/lightMovement.cs
@@ -43,15 +43,14 @@ public class lightMovement : MonoBehaviour
     private Rigidbody2D rb2D;
     TrailRenderer tr;
 
-    void Start()
-    {
+    void Start() {
         thisGameObject = gameObject;
         tr = thisGameObject.GetComponent<TrailRenderer>();
         rb2D = thisGameObject.GetComponent<Rigidbody2D>();
     }
-
-    public void OnMove(InputAction.CallbackContext context) 
-    {
+    
+    //* OnX functions receive the input call and transfer it to a variable
+    public void OnMove(InputAction.CallbackContext context) {
         moveAction = context.ReadValue<Vector2>();
     }
     public void OnJump(InputAction.CallbackContext context) {
@@ -61,16 +60,21 @@ public class lightMovement : MonoBehaviour
         dashAction = context.ReadValue<float>();
     } 
 
+    //* Move function moves the character with a certain amount of acceleration
     private void Move() {
+        //this way, when dashing, there will be no acceleration when
+        //coming out of the dash
         if(allowToDash == false) {
             speed = maximumSpeed;
         }
         else if (moveAction.x == 1 || moveAction.x == -1)
         {
+            //creates acceleration
             acceleration = maximumSpeed / accelerationTime;
             speed += acceleration * Time.deltaTime;
         }
         else {
+            //restarts the speed of the player
             speed = 0;
         }
         
@@ -81,16 +85,17 @@ public class lightMovement : MonoBehaviour
         Vector2 velocity = new Vector2(speed * moveAction.x * Time.deltaTime, rb2D.velocity.y);
         rb2D.velocity = velocity;    
         
-        }  
+    }  
     
+    //* Makes the jump possible
     private void Jump() {
         coyoteTimeCounter = 0;
         rb2D.velocity = new Vector2(rb2D.velocity.x, jumpHeight);
-        // rb2D.AddForce(new Vector2(rb2D.velocity.x, jumpHeight), ForceMode2D.Impulse);
     }    
-    //Suposed to do a especific jump when jumping at the same time that you dash
+    
+    //*Supposed to do a especific jump when jumping at the same time that you dash
     private IEnumerator DashJump() {
-
+        //defines the variables needed to do the dashjump and stop any other action
         allowToDash = false;
         isDashing = true;
         isJumping = true;
@@ -98,39 +103,43 @@ public class lightMovement : MonoBehaviour
         dashVelocity = 40f;
         rb2D.velocity = new Vector2(moveAction.x * rb2D.transform.localScale.x * dashVelocity, jumpHeight);
         tr.emitting = true;
+
+        //waits for the end of the dash
         yield return new WaitForSeconds(dashTime);
         yield return new WaitUntil(() => IsGrounded() == true);
+        
+        //restarts the variables
         tr.emitting = false;
         isDashing = false;
         isJumping = false;
         dashVelocity = 100f;
         jumpHeight = 15f;
     }
-
+    
+    //* The coroutine that makes the dash posible
     private IEnumerator Dash() {
-        Debug.Log("Dashing!");
+        //defines the variables needed to do the dash and stop any other action but jumping
         allowToDash = false;
         isDashing = true;
+        //increases the velocity
         rb2D.velocity = new Vector2(moveAction.x * rb2D.transform.localScale.x * dashVelocity, 0);
         tr.emitting = true;
-      
+        //restarts the variables
         yield return new WaitForSeconds(dashTime);
-        Debug.Log("dash Ended");
         isDashing = false;
         tr.emitting = false;
-        coyoteTimeCounter = 0;
-        
+        coyoteTimeCounter = 0;    
         amountOfDashes--;
-          if( !IsGrounded()) {
+
+        if( !IsGrounded()) {
             yield return new WaitUntil(() => IsGrounded() == true);
         }
         else {
             yield return new WaitForSeconds(dashingCooldown);
         }
-        
-        Debug.Log("cooldown off");
         allowToDash = true;
     }
+    
     //* casts a raycast under the player in the form of a box, to check if he's grounded or not
     public bool IsGrounded() {
         if (Physics2D.BoxCast(transform.position, boxSizeLight, 0, -transform.up, castDistanceLight, groundLayerLight)) {
@@ -140,11 +149,12 @@ public class lightMovement : MonoBehaviour
             return false;
         }
     }
-    //* makes the raycast visible.
+    //* makes the raycast visible in-Scene mode
     private void OnDrawGizmos() {
         Gizmos.DrawWireCube(transform.position - transform.up * castDistanceLight, boxSizeLight);
     }
-
+    
+    //* The Update variable is only used to define CoyoteTime
     private void Update() {
         if(IsGrounded()) {
             coyoteTimeCounter = coyoteTime;
@@ -153,36 +163,36 @@ public class lightMovement : MonoBehaviour
             accelerationTime = 0.6f;
             coyoteTimeCounter -= Time.deltaTime;
         }
-     
- 
     }
-    //* Fixed Update does all the calls for the movement functions, as they work with physics.
+    //* Fixed Update does all the calls for the movement functions
     private void FixedUpdate() {
-        if(allowToDash == true && isDashing == false && amountOfDashes < 3) {
-            amountOfDashes = amountOfDashes + 0.05f;
+        //will start the dashjump, or stop any other action, if already in dash
+        if(isDashing == true) {
+            if(jumpAction > 0 && coyoteTimeCounter > 0) {
+                StopCoroutine(Dash());
+                StartCoroutine(DashJump());
+            }
+            return;
         }
-
-        // if player is dashing, they can not move nor try to dash again.
-        if(isDashing == false && jumpAction > 0 && coyoteTimeCounter > 0) {
+        //It lets the dash start if called, and allows 
+        //the dash amount recharge
+        if(allowToDash == true) {
+            if(dashAction > 0 && moveAction.x != 0 && amountOfDashes >= 1) {
+                StartCoroutine(Dash());
+            }
+            if(amountOfDashes < 3) {
+                amountOfDashes = amountOfDashes + 0.05f;
+            }
+        }
+        
+        //activates the jump
+        if(jumpAction > 0 && coyoteTimeCounter > 0) {
             Jump();
         }
-        else if(isDashing == true && jumpAction > 0 && coyoteTimeCounter > 0) {
-            StopCoroutine(Dash());
-            StartCoroutine(DashJump());
-            return;
-        } 
-        
-        else if (isDashing == true){
-            return;
-        }
 
-        
+        //activates basic movement
         if(!isDashing && !isJumping) {
             Move();
-        }
-        
-        if(dashAction > 0 && allowToDash == true && moveAction.x != 0 && amountOfDashes >= 1) {
-            StartCoroutine(Dash());
         }
     }
 
