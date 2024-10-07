27478d55845ced415a073bcfd59cbd568dcf4de0
Guillermo del Toro Trillo <guillermotoro61@gmail.com>
2024-10-07 16:58:42 -0300

    Starting connection with Input Manager

--- GenericScripts/Interact.cs
+++ GenericScripts/Interact.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Events;
+using UnityEngine.InputSystem;
 using System;
 public class Interact : MonoBehaviour
 {
 
     public event Action Action;
-    
+    float lookAction;
+
+    void onInteraction(InputAction.CallbackContext context) {
+        lookAction = context.ReadValue<float>();
+    }
     // Update is called once per frame
     void Update()
     {
