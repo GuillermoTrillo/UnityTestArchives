668dd42666fc2607625ad05ead5f7b99a387db2d
Guillermo del Toro Trillo <guillermotoro61@gmail.com>
2024-10-07 16:56:44 -0300

    starting the interact function

---
+++ GenericScripts/Interact.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System;
+public class Interact : MonoBehaviour
+{
+
+    public event Action Action;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
