9cc1823c068573c46defb1e4410b9eb47b986c5f
Guillermo del Toro Trillo <guillermotoro61@gmail.com>
2024-10-09 16:36:32 -0300

    Reorganazing the scripts

---
+++ MapAssetsScripts/SavePointScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SavePointScript : MonoBehaviour
+{
+    public GameObject player;
+ private void Update() {
+      Debug.Log("Observer responds");
+ }      
+    
+        
+    
+    /*
+    private void Awake()
+    {
+        if (player != null)
+        {
+            player.interactAction += OnThingHappened;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (player != null)
+        {
+            player.interactAction -= OnThingHappened;
+      }
+    }
+    */
+}
